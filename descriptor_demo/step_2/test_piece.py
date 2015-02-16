6aedcc5af50d7948c5ba963724867459e00d9579
Matt Dougherty <thegeekofalltrades@gmail.com>
2015-02-16 13:48:43 -0800

    Add tests for validation.

--- descriptor_demo/step_2/test_piece.py
+++ descriptor_demo/step_2/test_piece.py
@@ -18,6 +18,50 @@ class TestPiece(unittest.TestCase):
         self.assertEqual(p.rank, 2)
         self.assertEqual(p.file, 4)
 
+    def test_valid_ranks(self):
+        """Pieces may be assigned ranks in the 0-8 range."""
+        p = Piece(Black)
+        for rank in range(9):
+            try:
+                p.rank = rank
+            except ValueError:
+                raise AssertionError(
+                    "Piece.rank could not be assigned the value"
+                    " {}".format(rank)
+                )
+
+    def test_valid_files(self):
+        """Pieces may be assigned files in the 0-8 range."""
+        p = Piece(Black)
+        for file in range(9):
+            try:
+                p.file = file
+            except ValueError:
+                raise AssertionError(
+                    "Piece.file could not be assigned the value"
+                    " {}".format(file)
+                )
+
+    def test_invalid_ranks(self):
+        """Pieces may not be assigned ranks outside the 0-8 range."""
+        p = Piece(Black)
+
+        with self.assertRaises(ValueError):
+            p.rank = -1
+
+        with self.assertRaises(ValueError):
+            p.rank = 9
+
+    def test_invalid_files(self):
+        """Pieces may not be assigned files outside the 0-8 range."""
+        p = Piece(Black)
+
+        with self.assertRaises(ValueError):
+            p.file = -1
+
+        with self.assertRaises(ValueError):
+            p.file = 9
+
 
 if __name__ == '__main__':
     unittest.main()
