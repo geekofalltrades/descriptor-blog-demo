374fab8b2bcab42507696e06261a165a7f4587df
Matt Dougherty <thegeekofalltrades@gmail.com>
2015-02-16 14:04:21 -0800

    Update chess piece with stratum and era.

--- descriptor_demo/step_3/piece.py
+++ descriptor_demo/step_3/piece.py
@@ -9,10 +9,12 @@ White = True
 class Piece(object):
     """A chess piece. Knows its color and location on the board."""
 
-    def __init__(self, color, rank=0, file=0):
+    def __init__(self, color, rank=0, file=0, stratum=0, era=0):
         self.color = color
         self.rank = rank
         self.file = file
+        self.stratum = stratum
+        self.era = era
 
     @property
     def rank(self):
@@ -33,3 +35,23 @@ class Piece(object):
         if value < 0 or value > 8:
             raise ValueError("file must be a value between 0 and 8, inclusive")
         self._file = value
+
+    @property
+    def stratum(self):
+        return self._stratum
+
+    @stratum.setter
+    def stratum(self, value):
+        if value < 0 or value > 8:
+            raise ValueError("stratum must be a value between 0 and 8, inclusive")
+        self._stratum = value
+
+    @property
+    def era(self):
+        return self._era
+
+    @era.setter
+    def era(self, value):
+        if value < 0 or value > 8:
+            raise ValueError("era must be a value between 0 and 8, inclusive")
+        self._era = value
