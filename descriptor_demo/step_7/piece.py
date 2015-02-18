e3eb1c93e9269d5a5c0fa8485fb9a0b49c094c4e
Matt Dougherty <thegeekofalltrades@gmail.com>
2015-02-17 16:57:25 -0800

    Generalize BoundedValue so that it can constrain to an arbitrary range.

--- descriptor_demo/step_7/piece.py
+++ descriptor_demo/step_7/piece.py
@@ -1,43 +1,51 @@
-"""Step 6: I experiment with a different way of tying descriptors to
-specific instances. Here, I've created a factory function that creates
-descriptors which store their value in a certain attribute on their
-class instance.
+"""Step 7: We refactor the BoundedValue so that it restricts its value
+to between arbitrary upper and lower bounds.
 """
 
 Black = False
 White = True
 
 
-def bounded_value_factory(name):
+def bounded_value_factory(name, lower, upper):
     """Factory function for BoundedValues. Creates BoundedValues which
     store their data in the named attribute on the instance in which
     they're accessed.
     """
 
     class BoundedValue(object):
-        """A value constrained to the 0-8 range, inclusive."""
+        """A value constrained to between given lower and upper bounds,
+        inclusive.
+        """
 
-        def __init__(self, name):
+        def __init__(self, name, lower, upper):
             self.name = name
+            self.lower = lower
+            self.upper = upper
 
         def __get__(self, instance, owner):
             return getattr(instance, self.name)
 
         def __set__(self, instance, value):
-            if value < 0 or value > 8:
-                raise ValueError("value must be between 0 and 8, inclusive")
+            if value < self.lower or value > self.upper:
+                raise ValueError(
+                    "value must be between {} and {}, inclusive".format(
+                        self.lower,
+                        self.upper
+                    )
+                )
+
             setattr(instance, self.name, value)
 
-    return BoundedValue(name)
+    return BoundedValue(name, lower, upper)
 
 
 class Piece(object):
     """A chess piece. Knows its color and location on the board."""
 
-    rank = bounded_value_factory('_rank')
-    file = bounded_value_factory('_file')
-    stratum = bounded_value_factory('_stratum')
-    era = bounded_value_factory('_era')
+    rank = bounded_value_factory('_rank', 0, 8)
+    file = bounded_value_factory('_file', 0, 8)
+    stratum = bounded_value_factory('_stratum', 0, 8)
+    era = bounded_value_factory('_era', -8, 8)
 
     def __init__(self, color, rank=0, file=0, stratum=0, era=0):
         self.color = color
